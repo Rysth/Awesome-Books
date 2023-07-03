c63344a55cdfff335930153bd0bd855e38f7259a
Rysth <johnpalacios.t@gmail.com>
2023-07-03 15:44:58 -0500

    Update scripts.js to simplify it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-// Create a collection that keeps a list of books (hint: you can use an array of objects for that).
-let booksArray = [];
-
-// Create a function to add a new book to the collection, with title and author.
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -9,50 +5,14 @@ class Book {
   }
 }
 
+let booksArray = [];
+
 function addBook(title, author) {
   const book = new Book(title, author);
   booksArray.push(book);
   updateLocalStorage();
-}
-
-function showBook(library, book, index) {
-  const newBook = `
-  <div class="book">
-    <div class="book-cover">
-      <h2 class="book-title">${book.title}</h2>
-      <p class="book-author">${book.author}</p>
-    </div>
-    <button class="btn btn-danger float-right" type="button" onclick="removeBook(${index})">Remove</button>
-    <hr />
-  </div>
-  `;
-
-  library.innerHTML += newBook;
-}
-
-function displayBooks() {
-  const library = document.querySelector('#library');
-  library.innerHTML = '';
-  booksArray.forEach((book, index) => {
-    showBook(library, book, index);
-  });
-}
-
-const form = document.querySelector('#form');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-  addBook(title, author);
-
   displayBooks();
-
-  form.reset();
-});
-
-// Create a function to remove a book from the collection (hint: you can use the array filter() method).
+}
 
 function removeBook(index) {
   booksArray.splice(index, 1);
@@ -64,17 +24,47 @@ function updateLocalStorage() {
   localStorage.setItem('booksArray', JSON.stringify(booksArray));
 }
 
+function showBooks(library) {
+  library.innerHTML = booksArray
+    .map(
+      (book, index) => `
+          <div class="book">
+            <div class="book-cover">
+              <h2 class="book-title">${book.title}</h2>
+              <p class="book-author">${book.author}</p>
+            </div>
+            <button class="btn btn-danger float-right" type="button" onclick="removeBook(${index})">Remove</button>
+            <hr />
+          </div>
+  `
+    )
+    .join('');
+}
+
+function displayBooks() {
+  const library = document.querySelector('#library');
+  showBooks(library);
+}
+
 function checkLocalStorage() {
-  const item = localStorage.getItem('booksArray');
-  console.log(item);
-  if (item !== null) {
-    const data = JSON.parse(item);
-    if (data.length <= 0) return localStorage.clear();
-    booksArray = data;
+  const storedData = localStorage.getItem('booksArray');
+  if (storedData !== null) {
+    const data = JSON.parse(storedData);
+    booksArray.splice(0, booksArray.length, ...data);
     displayBooks();
   }
 }
 
-window.onload = () => {
-  checkLocalStorage();
-};
+window.onload = checkLocalStorage;
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  addBook(title, author);
+
+  form.reset();
+});
